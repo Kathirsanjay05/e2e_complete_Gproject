ea0bfe510b8a9aa753db06bbadfe3c5a406536b1
Kathir sanjay <kathirsanjay5@gmail.com>
2026-04-26 07:38:13 +0530

    setup.py ,requirement.txt and src folder

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages,setup # type: ignore
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+
+
+
+def get_requirements(file_path:str)->List[str]:
+
+    '''
+    This function will read and return the requirements packages
+
+    '''
+
+    requirements=[]
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [i.replace("\n","") for i in requirements] 
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+
+## like a metadata 
+setup(
+    name='complete_e2e_project',
+    version='0.0.1',
+    author='kathir sanjay',
+    author_email='kathirsanjay5@gmail.com',
+    packages = find_packages(), # it directly go and see every folder and folder which 
+    # has __init__.py it directly consider the folder(src) aspackage and build this 
+    install_requires = get_requirements('requirements.txt')
+)
